39fd1f278c790fb894d07d665dfa56551499e958
Ionut Andrei <web@imandy.ie>
2017-07-31 21:23:15 +0100

    Add material ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { url } from './settings';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './styles/css/app.css';
 
 import Home from './routes/Home';
@@ -10,19 +12,47 @@ import SinglePost from './routes/SinglePost';
 import Login from './routes/Login';
 import Register from './routes/Register';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.unsubscribeStore = store.subscribe(this.handleStoreChange);
+  }
+
+  componentDidUnmount() {
+    this.unsubscribeStore();
+  }
+
+  handleStoreChange() {
+    let isLoadingState = store.getState().inProgress;
+    if (isLoadingState !== this.state.isLoading) {
+      this.setState({ isLoading: isLoadingState });
+    }
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <Router basename={url.basename}>
-          <div>
-            <Route exact={true} path="/" component={Home}/>
-            <Route exact={true} path="/post" component={SinglePost}/>
-            <Route exact={true} path="/login" component={Login}/>
-            <Route exact={true} path="/register" component={Register}/>
-          </div>
-        </Router>
-      </Provider>
+      <MuiThemeProvider>
+        <Provider store={store}>
+          <Router basename={url.basename}>
+            <div>
+              <Route exact={true} path="/" component={Home}/>
+              <Route exact={true} path="/post" component={SinglePost}/>
+              <Route exact={true} path="/login" component={Login}/>
+              <Route exact={true} path="/register" component={Register}/>
+            </div>
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
     );
   }
 }
